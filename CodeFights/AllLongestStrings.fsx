8827bba824aedd0513802d663c0283e3c5383510
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-09-25 13:25:00 +0200

    common characters

---
+++ CodeFights/AllLongestStrings.fsx
@@ -0,0 +1,17 @@
+﻿#load "Imports.fsx"
+open Swensen.Unquote
+
+let orElse def value = defaultArg value def
+let allLongestStrings inputArray =
+    inputArray
+    |> Array.groupBy Seq.length
+    |> Array.sortByDescending fst
+    |> Array.tryHead
+    |> Option.map snd
+    |> orElse [||]
+
+printf "testing.."
+test <@ allLongestStrings [||] |> Array.isEmpty @>
+test <@ [|"a";"b";"c"|] |> allLongestStrings = [|"a";"b";"c"|] @>
+test <@ [|"aa";"b";"c"|] |> allLongestStrings = [|"aa"|] @>
+printfn "..done!"
---
+++ CodeFights/CommonCharacterCount.fsx
@@ -0,0 +1,24 @@
+﻿#load "Imports.fsx"
+open Swensen.Unquote
+
+type State = {common : char list; other : char list}
+
+let rec stripOne acc letter list = 
+    match list with 
+    | [] -> acc
+    | h :: t when letter = h -> List.append acc t
+    | h :: t -> stripOne (h :: acc) letter t
+
+let commonCharacterCount s1 s2 =
+    let folder state letter = 
+        if state.other |> Seq.contains letter then
+            {state with common = letter :: state.common; other = state.other |> stripOne [] letter}
+        else
+            state
+    s1
+    |> Seq.fold folder {common = []; other = s2 |> Seq.toList }
+    |> fun state -> state.common |> List.length
+
+printf "testing.."
+test <@ commonCharacterCount "abc" "abc" = 3 @>
+printfn "..done!"
