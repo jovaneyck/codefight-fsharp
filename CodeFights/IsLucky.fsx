d9c84b41c43f4c57aa3db99ecafb7a3dedc61e51
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-09-25 13:54:37 +0200

    lucky numbers

---
+++ CodeFights/IsLucky.fsx
@@ -0,0 +1,23 @@
+﻿#load "Imports.fsx"
+open Swensen.Unquote
+
+let digits number =
+    let rec digits acc number =
+        if number < 10 then
+            number :: acc
+        else
+            let digit = number % 10
+            digits (digit :: acc) (number / 10)
+    digits [] number
+
+let isLucky number = 
+    let [first; second] =
+        number
+        |> digits
+        |> List.splitInto 2
+    (first |> List.sum) = (second |> List.sum)
+
+printf "testing.."
+test <@ isLucky 1230 @>
+test <@ isLucky 239017 |> not @>
+printfn "..done!"
