62ed1648371346cdc1367fadc338da86a7a69b2c
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-09-29 08:12:57 +0200

    sort by height trees persons

---
+++ CodeFights/sortByHeight.fsx
@@ -0,0 +1,26 @@
+﻿#load "Imports.fsx"
+open Swensen.Unquote
+
+type Space = Tree | Free | Person of int
+
+let rec addToPark park person =
+    match park with
+    | Free :: p -> (Person person) :: p
+    | other :: p -> other :: (addToPark p person)
+
+let rec toNumbers park =
+    match park with
+    | [] -> []
+    | Tree :: p -> -1 :: (toNumbers p)
+    | Person p :: rest -> p :: (toNumbers rest)
+
+let sortByHeight array = 
+    let a = array |> List.ofArray
+    let parkLayout = a |> List.map (fun n -> if n = -1 then Tree else Free)
+    let sortedPeople = a |> List.filter (fun n -> n <> -1) |> List.sort
+    let sortedPark = sortedPeople |> List.fold addToPark parkLayout
+    sortedPark |> toNumbers |> List.toArray
+
+printf "testing.."
+test <@ sortByHeight [|-1; 150; 190; 170; -1; -1; 160; 180|] = [|-1; 150; 160; 170; -1; -1; 180; 190|] @>
+printfn "..done!"
