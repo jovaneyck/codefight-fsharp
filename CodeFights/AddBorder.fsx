b4837adb2f620aeac3a675d8806e20d56001e5f8
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-10-06 13:05:45 +0200

    addBorder

---
+++ CodeFights/AddBorder.fsx
@@ -0,0 +1,15 @@
+﻿#load "Imports.fsx"
+open Swensen.Unquote
+
+let addBorder picture =
+    let width = picture |> Array.head |> Seq.length
+    let fullWidthFrame = Seq.replicate (width + 2) "*" |> String.concat "" |> Array.singleton
+    let innerLines = picture |> Array.map (sprintf "*%s*")
+    Array.concat [fullWidthFrame; innerLines; fullWidthFrame]
+
+let picture = [|"abc";"ded"|]
+printf "testing.."
+
+test <@ addBorder picture = [| "*****"; "*abc*"; "*ded*"; "*****"|] @>
+
+printf "..done"
