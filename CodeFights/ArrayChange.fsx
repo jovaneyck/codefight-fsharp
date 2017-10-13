ca7f1ad0c611576e08f9cf3cb852a13988bdadab
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-10-13 15:28:09 +0200

    array change how many steps to make strictly increasing array

---
+++ CodeFights/ArrayChange.fsx
@@ -0,0 +1,22 @@
+﻿#load "Imports.fsx"
+open Swensen.Unquote
+
+type State = {previousValue : int; totalIncrement : int}
+
+let arrayChange a = 
+    let folder state element = 
+        let diff =
+            if element > state.previousValue then 0
+            else 1 + (state.previousValue - element)
+        {previousValue = element + diff; totalIncrement = state.totalIncrement + diff}
+
+    a 
+    |> List.ofArray 
+    |> List.fold folder {previousValue = System.Int32.MinValue; totalIncrement = 0}
+    |> (fun state -> state.totalIncrement)
+
+printf "Testing..."
+
+test <@ arrayChange [|1;1;1|] = 3 @>
+
+printfn "..done"
